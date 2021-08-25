1f4fc5503f6efee2205b59747f36c59f0c7a56d8
= <=>
2021-08-25 17:18:08 +0500

    server

--- index.js
+++ index.js
@@ -3,9 +3,78 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('<h1>Homepage</h1>')
+  // if (req.url === '/') {
+  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+  //     if (err) throw err;
+  //     res.writeHead(200, { 'Content-Type': 'text/html' })
+  //     res.end(content)
+  //   })
+  // }
+
+  // if (req.url === '/about') {
+  //   fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+  //     if (err) throw err;
+  //     res.writeHead(200, { 'Content-Type': 'text/html' })
+  //     res.end(content)
+  //   })
+  // }
+
+  // if (req.url === '/api/users') {
+  //   const users = [
+  //     { name: 'Haroon', age: 22 },
+  //     { name: 'Ali', age: 37 },
+  //     { name: 'Umer', age: 20 }
+  //   ];
+
+  //   res.writeHead(200, { 'Content-Type': 'application/json' });
+  //   res.end(JSON.stringify(users))
+  // }
+
+  // Building file path
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+  // Extension of file
+  let extname = path.extname(filePath);
+
+  let contentType = 'text/html';
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript'
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+      case '.json':
+        contentType = 'application/json';
+        break;
+      case '.png':
+        contentType = 'image/png';
+        break;
+      case '.jpg':
+        contentType = 'image/jpg';
+        break;
+    default:
+      break;
   }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        })
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`)
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(content, 'utf-8');
+    }
+  })
+
 })
 
 const PORT = process.env.PORT || 5000;
